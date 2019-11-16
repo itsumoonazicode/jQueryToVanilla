439b5b6255910caadd37767ab22f4e3efdca0126
itsumoonazicode <dyingkouki@gmail.com>
2019-11-17 02:27:48 +0900

    initial commit

---
+++ mousewheelevent/main.js
@@ -0,0 +1,18 @@
+$(function () {
+	scrLength = 350;
+	scrSpeed = 600;
+	scrEasing = 'easeOutCirc';
+
+	var mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+	$(document).on(mousewheelevent, function (e) {
+		e.preventDefault();
+		var delta = e.originalEvent.deltaY ? -(e.originalEvent.deltaY) : e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta : -(e.originalEvent.detail);
+		if (delta < 0) {
+			scrSet = $(document).scrollTop() + scrLength;
+		} else {
+			scrSet = $(document).scrollTop() - scrLength;
+		}
+		$('html,body').stop().animate({ scrollTop: scrSet }, scrSpeed, scrEasing);
+		return false;
+	});
+});
